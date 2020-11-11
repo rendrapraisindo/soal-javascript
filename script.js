a4a7a5b9dcd7ff651a5fdcaf034d775148af483a
Muhamad Rendra <37527416+rendrapraisindo@users.noreply.github.com>
2020-11-11 19:19:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* 
+Tugas Javascript
+Nama : __________(isi nama kalian)
+
+Instruksi : 
+1. Buat agar halaman index.html, jika dibuka di browser chrome / firefox versi terakhir dapat menampilkan seperti pada video
+2. Dilarang mengubah file index.html
+3. Yang dikumpulkan hanya file javascript ini saja
+4. Tidak boleh menggunakan framework javascript apapun (misal jquery)
+*/
